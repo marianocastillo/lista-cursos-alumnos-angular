8b361fd26e2aaa24e879ac15a953bfb9af988f1a
Marianocastillo <marianocastillo08@gmail.com>
2023-11-22 01:19:10 -0400

    lista de alumnos y cursos

---
+++ src/app/components/tabla-alumnos/mock.ts
@@ -0,0 +1,27 @@
+import { Alumno } from "src/app/models/alumno-to"
+
+export const alumnos: Alumno[] = [
+    {
+        nombre: 'Yesenia',
+        apellido: 'perez',
+        promedio: 8.5
+    }, 
+    
+    {
+        nombre: 'Pedro',
+        apellido: 'Lajara',
+        promedio: 10.5
+    }, 
+    
+    {
+        nombre: 'Moises',
+        apellido: 'Vazques',
+        promedio: 5.3
+    }, 
+    
+    {
+        nombre: 'Paola',
+        apellido: 'Montas',
+        promedio: 9.3
+    }
+]
---
+++ src/app/components/tabla-cursos/mock.ts
@@ -0,0 +1,27 @@
+import { Curso } from "src/app/models/curso-to";
+
+export const cursos: Curso[] = [
+    {
+        materia: 'Javascript',
+        dificultad: 9,
+        requiere: 'html y css'
+    }, 
+    
+    {
+        materia: 'PHP',
+        dificultad: 10,
+        requiere: 'html y javascript'
+    }, 
+    
+    {
+        materia: 'Python',
+        dificultad: 6,
+        requiere: 'django y javascript'
+    }, 
+    
+    {
+        materia: 'Angular',
+        dificultad: 3,
+        requiere: 'typescript y javascript'
+    }
+]
